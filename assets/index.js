775b6f8b19608e21714ce52def63cd593b9e3139
LuismGil <luism6977@gmail.com>
2021-08-28 18:30:16 -0300

    Adding plugins

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,10 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
+
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause()] });
 
 const playButton = document.querySelector('#playButton');
 playButton.onclick = () => player.togglePlay();
