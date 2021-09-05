e64fff2579e3de352da9162e55974998f2c0a9b9
LuismGil <luism6977@gmail.com>
2021-09-05 15:26:41 -0300

    Added Ads plugin

--- assets/index.ts
+++ assets/index.ts
@@ -1,10 +1,11 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/Ads'
 
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause()] });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause(), new Ads()] });
 
 const playButton: HTMLElement = document.querySelector('#playButton');
 playButton.onclick = () => player.togglePlay();
